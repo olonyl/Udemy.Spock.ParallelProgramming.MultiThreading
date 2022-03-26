66be52b0ded68c433f0e3ae953cdfa5353a63af4
Olonyl Landeros <jose87.landeros@gmail.com>
2022-03-25 19:01:02 -0600

    Added the Process Library

---
+++ 13.CreateProcess/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _13.CreateProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = @"C:\temp\HelloWorld.txt";
+            var progname = "notepad.exe";
+
+            Process.Start(progname, filename);
+            Process.Start(filename);
+
+            var app = new Process
+            {
+                StartInfo =
+                {
+                    FileName=progname,
+                    Arguments=filename
+                }
+            };
+            app.Start();
+            app.PriorityClass = ProcessPriorityClass.RealTime;
+
+            app.WaitForExit();
+
+            Console.WriteLine("No more waiting");
+
+            var process = Process.GetProcesses();
+            foreach (var proc in process)
+            {
+                if (proc.ProcessName == "notepad")
+                {
+                    proc.Kill();
+                }
+            }
+            Console.Read();
+        }
+    }
+}
