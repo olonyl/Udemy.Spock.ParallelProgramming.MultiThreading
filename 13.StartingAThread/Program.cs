76008bad82e96b8280b216f08b689ce191f40727
Olonyl Landeros <jose87.landeros@gmail.com>
2022-03-25 19:16:07 -0600

    Starting a Thread

---
+++ 13.StartingAThread/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace _13.StartingAThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(() => Print(true));
+            t1.Start();
+
+            Print(true);
+
+            Console.Read();
+        }
+        private static void Print(bool isEven)
+        {
+            Console.WriteLine($"Current Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+            if (isEven)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (i % 2 == 0 && isEven)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+
+        }
+    }
+
+}
