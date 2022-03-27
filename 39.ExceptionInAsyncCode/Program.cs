cde01e9fe5b73984f84da8ef8e0f9ce3a0fc6e41
Olonyl Landeros <jose87.landeros@gmail.com>
2022-03-27 17:10:37 -0600

    Exception Handling inside Async

--- 39.ExceptionInAsyncCode/Program.cs
+++ 39.ExceptionInAsyncCode/Program.cs
@@ -7,8 +7,30 @@ namespace _39.ExceptionInAsyncCode
     {
         static void Main(string[] args)
         {
-            Catcher();
+            //Catcher();
+            CastMultipleExceptionsWithAwait();
             Console.Read();
+        }
+
+        private async static void CastMultipleExceptionsWithAwait()
+        {
+            int[] numbers = { 0 };
+
+            Task<int> t1 = Task.Run(() => 5 / numbers[0]);
+            Task<int> t2 = Task.Run(() => numbers[1]);
+
+            Task<int[]> allTasks = Task.WhenAll(t1, t2);
+            try
+            {
+                await allTasks;
+            }
+            catch (Exception)
+            {
+                foreach (var ex in allTasks.Exception.InnerExceptions)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
         }
 
         private static async Task Catcher()
