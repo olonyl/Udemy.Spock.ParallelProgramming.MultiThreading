48af7b62ddafcefca0b95adccbe2fd7750e6702d
Olonyl Landeros <jose87.landeros@gmail.com>
2022-03-27 16:48:53 -0600

    Introducing Task Async Await

---
+++ 39.ExceptionInAsyncCode/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _39.ExceptionInAsyncCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Catcher();
+            Console.Read();
+        }
+
+        private static async Task Catcher()
+        {
+            try
+            {
+                var thrower = Thrower();
+                await thrower;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        async static Task Thrower()
+        {
+            await Task.Delay(1000);
+            throw new InvalidOperationException();
+        }
+    }
+}
