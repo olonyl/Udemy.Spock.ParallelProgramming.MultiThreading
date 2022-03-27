fa29780ef94b097d1f700a4eb9443de0857896a3
Olonyl Landeros <jose87.landeros@gmail.com>
2022-03-26 20:44:59 -0600

    Understanding how Tasks works when it comes to ThreadPool, this can be controller by setting TaskcreationOptions to LongRunning

---
+++ 22.StartingATask/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _22.StartingATask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> t1 = Task.Factory.StartNew(() => Print(true)
+            , CancellationToken.None
+            , TaskCreationOptions.DenyChildAttach | TaskCreationOptions.LongRunning
+            , TaskScheduler.Default);
+
+            Task<int> t2 = Task.Factory.StartNew(() => Print(false));
+
+
+            Console.WriteLine($"Task #1 Result={t1.Result}");
+            Console.WriteLine($"Task #2 Result={t2.Result}");
+
+            Console.ReadLine();
+        }
+
+        private static int Print(bool isEven)
+        {
+            Console.WriteLine($"Task {Task.CurrentId} Is Thread Pool Thread= {Thread.CurrentThread.IsThreadPoolThread}");
+            int total = 0;
+            if (isEven)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        total++;
+                        Console.WriteLine($"Current task id={Task.CurrentId}, Value={i}");
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        total++;
+                        Console.WriteLine($"Current task id={Task.CurrentId}, Value={i}");
+                    }
+                }
+            }
+            return total;
+        }
+    }
+
+}
