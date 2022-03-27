7696057d7999f6efd251579ac71a532c7d2b3265
Olonyl Landeros <jose87.landeros@gmail.com>
2022-03-27 13:50:57 -0600

    Using TaskCompletionSource

---
+++ 33.TaskCompletionSource/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _33.TaskCompletionSourceExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> task = Run(() =>
+            {
+                Thread.Sleep(5000);
+                return 42;
+            });
+
+            Console.WriteLine(task.Result);
+            Console.Read();
+        }
+        private static Task<TResult> Run<TResult>(Func<TResult> function)
+        {
+            var tcs = new TaskCompletionSource<TResult>();
+            new Thread(() =>
+            {
+                try
+                {
+                    tcs.SetResult(function());
+                }
+                catch (Exception ex)
+                {
+                    tcs.SetException(ex);
+                }
+            }).Start();
+            return tcs.Task;
+        }
+    }
+
+
+}
