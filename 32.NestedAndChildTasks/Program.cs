52b751a886bbbce79cdd845d197f6fe6142ee941
Olonyl Landeros <jose87.landeros@gmail.com>
2022-03-27 13:42:45 -0600

    Implemented nested child task

---
+++ 32.NestedAndChildTasks/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _32.NestedAndChildTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                Task nested = Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("hello world!");
+                    Thread.Sleep(4000);
+                });
+
+            }, TaskCreationOptions.AttachedToParent).Wait();
+
+            Console.WriteLine("Main");
+            Console.ReadLine();
+        }
+    }
+}
