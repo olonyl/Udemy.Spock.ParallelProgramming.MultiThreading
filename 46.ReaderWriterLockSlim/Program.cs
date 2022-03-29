6e4839e40cb9c6ebf893c7adbcd31032ec8b7821
Olonyl Landeros <jose87.landeros@gmail.com>
2022-03-28 20:49:14 -0600

    Implemented ReaderWriterLockSlim

---
+++ 46.ReaderWriterLockSlim/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace _46.ReaderWriterLockSlimExample
+{
+    class BankCard
+    {
+        private decimal _moneyAount;
+        private decimal _credit;
+        private readonly object _token = new object();
+        private ReaderWriterLockSlim _rwLock = new ReaderWriterLockSlim();
+
+        public BankCard(decimal moneyAmount)
+        {
+            _moneyAount = moneyAmount;
+        }
+
+        public decimal TotalMoneyAmount
+        {
+            get
+            {
+                using (_rwLock.TakeReaderLock(TimeSpan.FromSeconds(3)))
+                {
+                    return _moneyAount + _credit;
+                }
+            }
+        }
+        public void ReceivedPayment(decimal amount)
+        {
+            using (_rwLock.TakeWriteLock(TimeSpan.FromSeconds(3)))
+            {
+                _moneyAount += amount;
+            }
+        }
+
+        public void TransferToCard(decimal amount, BankCard recipient)
+        {
+            lock (_token)
+            {
+                _moneyAount -= amount;
+
+                recipient.ReceivedPayment(amount);
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 46.ReaderWriterLockSlim/ReaderWriterLockSlimExampleExtesnsions.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading;
+
+namespace _46.ReaderWriterLockSlimExample
+{
+    public static class ReaderWriterLockSlimExampleExtesnsions
+    {
+        public static ReaderLockSlimWrapper TakeReaderLock(this ReaderWriterLockSlim rwlock, TimeSpan timeout)
+        {
+            bool taken = false;
+            try
+            {
+                taken = rwlock.TryEnterReadLock(timeout);
+                if (taken)
+                {
+                    return new ReaderLockSlimWrapper(rwlock);
+                }
+                throw new TimeoutException("Failed to acquire a ReadWriterLockSlim object in read mode");
+            }
+            catch (Exception)
+            {
+                if (taken)
+                {
+                    rwlock.ExitReadLock();
+                }
+                throw;
+            }
+
+        }
+
+        public static WriterLockSlimWrapper TakeWriteLock(this ReaderWriterLockSlim rwlock, TimeSpan timeout)
+        {
+            bool taken = false;
+            try
+            {
+                taken = rwlock.TryEnterWriteLock(timeout);
+                if (taken)
+                {
+                    return new WriterLockSlimWrapper(rwlock);
+                }
+                throw new TimeoutException("Failed to acquire a ReadWriterLockSlim object in write mode");
+            }
+            catch (Exception)
+            {
+                if (taken)
+                {
+                    rwlock.ExitWriteLock();
+                }
+                throw;
+            }
+
+        }
+    }
+
+    public struct ReaderLockSlimWrapper : IDisposable
+    {
+        private readonly ReaderWriterLockSlim _rwlock;
+
+        public ReaderLockSlimWrapper(ReaderWriterLockSlim rwlock)
+        {
+            _rwlock = rwlock;
+        }
+
+        public void Dispose()
+        {
+            _rwlock.ExitReadLock();
+        }
+    }
+
+    public struct WriterLockSlimWrapper : IDisposable
+    {
+        private readonly ReaderWriterLockSlim _rwlock;
+
+        public WriterLockSlimWrapper(ReaderWriterLockSlim rwlock)
+        {
+            _rwlock = rwlock;
+        }
+
+        public void Dispose()
+        {
+            _rwlock.ExitWriteLock();
+        }
+    }
+}
