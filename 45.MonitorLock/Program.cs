1079cdf7c78593010571b5153e1d6b780cb02a8d
Olonyl Landeros <jose87.landeros@gmail.com>
2022-03-28 20:13:02 -0600

    Using lock Monitor Extension Method - object along with Monitor
    All of this to ensure Thread Safety

---
+++ 45.MonitorLock/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+
+namespace _45.MonitorLock
+{
+    class BankCard
+    {
+        private decimal _moneyAount;
+        private readonly object _token = new object();
+
+        public BankCard(decimal moneyAmount)
+        {
+            _moneyAount = moneyAmount;
+        }
+
+        public void ReceivedPayment(decimal amount)
+        {
+            bool lockTaken = false;
+            try
+            {
+
+                Monitor.Enter(_token, ref lockTaken);
+
+                _moneyAount += amount;
+
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    Monitor.Exit(_token);
+                }
+            }
+        }
+
+        public void TransferToCard(decimal amount, BankCard recipient)
+        {
+            lock (_token)
+            {
+                _moneyAount -= amount;
+
+                recipient.ReceivedPayment(amount);
+            }
+        }
+
+        public void DebitMoney(decimal amount)
+        {
+            using (_token.Lock(TimeSpan.FromSeconds(3)))
+            {
+                _moneyAount -= amount;
+
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
