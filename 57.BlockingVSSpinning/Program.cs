e77cd9e96984c829922b3e589f5ded11e3db8e87
Olonyl Landeros <jose87.landeros@gmail.com>
2022-03-29 15:18:42 -0600

    Implemented Spin.Wait to wait for a variable to change

---
+++ 57.BlockingVSSpinning/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _57.BlockingVSSpinning
+{
+    class Program
+    {
+        private static bool _done;
+        private static void MultiplyXByY(ref int val, int factor)
+        {
+            var spinWait = new SpinWait();
+            while (true)
+            {
+                int snapshot1 = val;
+                int calc = snapshot1 * factor;
+                int snapshot2 = Interlocked.CompareExchange(ref val, calc, snapshot1);
+                if (snapshot1 == snapshot2)
+                {
+                    return;
+                }
+                spinWait.SpinOnce();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Task.Run(() =>
+            {
+                try
+                {
+                    Console.WriteLine("Task started.");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Task is done.");
+                }
+                finally
+                {
+                    _done = true;
+                }
+            });
+
+            SpinWait.SpinUntil(() =>
+            {
+                Thread.MemoryBarrier();
+                return _done;
+            });
+
+            Console.WriteLine("The end of program");
+            Console.Read();
+        }
+    }
+}
