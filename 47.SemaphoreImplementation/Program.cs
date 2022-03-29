66eebe5f02d9632b2dab2387585a1361b73c701b
Olonyl Landeros <jose87.landeros@gmail.com>
2022-03-28 21:16:06 -0600

    Implemented Semaphore

---
+++ 47.SemaphoreImplementation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _47.SemaphoreImplementation
+{
+    class Program
+    {
+        public static SemaphoreSlim Bouncer { get; set; }
+        static void Main(string[] args)
+        {
+            Bouncer = new SemaphoreSlim(3, 3);
+
+            OpenNightClub();
+
+            Console.Read();
+        }
+
+        private static void OpenNightClub()
+        {
+            for (int i = 1; i <= 50; i++)
+            {
+                var number = i;
+                Task.Run(() => Guest(number));
+            }
+        }
+
+        private static void Guest(int number)
+        {
+            Console.WriteLine($"Guest {number} is waiting to entering nightclub");
+            Bouncer.Wait();
+
+            Console.WriteLine($"Guest {number} is doing some dancing");
+            Thread.Sleep(500);
+
+            Console.WriteLine($"Guest {number} is leaving the nightclub");
+            Bouncer.Release();
+        }
+    }
+}
