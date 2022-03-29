926d35d8571289fafa22ea9f35e1c7e02e20fa0c
Olonyl Landeros <jose87.landeros@gmail.com>
2022-03-29 17:44:24 -0600

    Implemented Immutable Stacks

---
+++ 63.ImplementingImmutableStack/IStack.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace _63.ImplementingImmutableStack
+{
+    public interface IStack<T> : IEnumerable<T>
+    {
+        IStack<T> Push(T value);
+        IStack<T> Pop();
+        T Peek();
+        bool IsEmpty { get; }
+    }
+}
---
+++ 63.ImplementingImmutableStack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _63.ImplementingImmutableStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IStack<int> stack = _63.ImplementingImmutableStack.Stack<int>.Empty;
+
+            IStack<int> stack2 = stack.Push(10);
+            IStack<int> stack3 = stack2.Push(20);
+            IStack<int> stack4 = stack3.Push(30);
+
+            foreach (var cur in stack4)
+            {
+                Console.WriteLine(cur);
+            }
+
+            Console.Read();
+        }
+
+    }
+}
