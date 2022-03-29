13cbb6185fd9d9c926d136875cda32099a7474a5
Olonyl Landeros <jose87.landeros@gmail.com>
2022-03-28 21:41:24 -0600

    Implemented logic to show Deadlocks

---
+++ 49.DeadLock/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _49.DeadLock
+{
+    class Program
+    {
+        static readonly object firstLock = new object();
+        static readonly object secondLock = new object();
+
+        static void Main(string[] args)
+        {
+            Task.Run(Do);
+
+            AcquireInversed();
+            Console.Read();
+        }
+
+        private static void AcquireInversed()
+        {
+            Thread.Sleep(500);
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}-Locking secondLock");
+            lock (secondLock)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}-Locked secondLock");
+                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}-Locking firstLock");
+                lock (firstLock)
+                {
+                    Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}-Locked firstLock");
+                }
+                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}-Released firstLock");
+            }
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}-Released secondLock");
+        }
+
+        private static void Do()
+        {
+            Console.WriteLine($"\t\t\t\t{Thread.CurrentThread.ManagedThreadId}-Locking firstLock");
+            lock (firstLock)
+            {
+                Console.WriteLine($"\t\t\t\t{Thread.CurrentThread.ManagedThreadId}-Locked firstLock");
+
+                Thread.Sleep(1000);
+
+                Console.WriteLine($"\t\t\t\t{Thread.CurrentThread.ManagedThreadId}-Locking secondLock");
+                lock (secondLock)
+                {
+                    Console.WriteLine($"\t\t\t\t{Thread.CurrentThread.ManagedThreadId}-Locked secondLock");
+                }
+                Console.WriteLine($"\t\t\t\t{Thread.CurrentThread.ManagedThreadId}-Released secondLock");
+            }
+            Console.WriteLine($"\t\t\t\t{Thread.CurrentThread.ManagedThreadId}-Released fisrtLock");
+        }
+    }
+}
